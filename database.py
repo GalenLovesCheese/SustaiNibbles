e3d522d8d4a21f5a91b5df1a48fc2d34780a9ab9
elphabetsoop <s10257905@connect.np.edu.sg>
2024-12-21 20:25:54 +0800

    feat: working announcement

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-#Paste this code under mycursor on first launch
 import mysql.connector
 import os
 from dotenv import load_dotenv
@@ -10,13 +9,12 @@ mydb = mysql.connector.connect(
     host="localhost",
     user="user",
     password = PASS,
-    database = "sustainibbles"
+    database = "table2"#"sustainibbles"
 )
 
 mycursor = mydb.cursor()
-
-mycursor.execute("CREATE DATABASE IF NOT EXISTS sustainibbles")
+mycursor.execute("CREATE DATABASE IF NOT EXISTS table2")
 mycursor.execute("CREATE TABLE IF NOT EXISTS Users (User VARCHAR(255), Type VARCHAR(255) CHECK(Type = 'Individual' OR Type = 'Business'))")
 mycursor.execute("CREATE TABLE IF NOT EXISTS Announcements (Location VARCHAR(255), Message VARCHAR(255), PAX int)")
-mycursor.execute("INSERT INTO Users(User, Type) VALUES('Ben', 'Individual'),('Thomas', 'Individual'),('Margaret', 'Individual'),('Dumping Donuts', 'Business'), ('Ivy Cafe','Business')")
+mycursor.execute("INSERT INTO Users(User, Type) VALUES('Ben', 'Individual'),('Thomas', 'Individual'),('Margaret', 'Individual'),('Dumping Donuts', 'Business'), ('Ivy Cafe','Business'), ('1793678228', 'Business')")
 mycursor.execute("INSERT INTO Announcements(Location, Message, PAX) VALUES('Bukit Panjang', 'Extra rice left over at store, up to 5 people can  take', 5), ('King Albert Park', 'Extra prata remaining', 2), ('Choa Chu Kang', 'Extra chicken remaining', 3)")
--- main.py
+++ main.py
@@ -15,29 +15,34 @@ mydb = mysql.connector.connect(
     host="localhost",
     user="user",
     password = PASS,
-    database = "table1"
+    database = "table2" #"sustainibbles"
 )
 
 mycursor = mydb.cursor()
-#mycursor.execute("CREATE DATABASE table1")
-# mycursor.execute("CREATE TABLE Users (User VARCHAR(255), Type VARCHAR(255) CHECK(Type = 'Individual' OR Type = 'Business'))")
-# mycursor.execute("CREATE TABLE Announcements (Location VARCHAR(255), Message VARCHAR(255), PAX int)")
+mycursor.execute("CREATE DATABASE IF NOT EXISTS table2")
+mycursor.execute("CREATE TABLE IF NOT EXISTS Users (User VARCHAR(255), Type VARCHAR(255) CHECK(Type = 'Individual' OR Type = 'Business'))")
+mycursor.execute("CREATE TABLE IF NOT EXISTS Announcements (Location VARCHAR(255), Message VARCHAR(255), PAX int)")
 mycursor.execute("INSERT INTO Users(User, Type) VALUES('Ben', 'Individual'),('Thomas', 'Individual'),('Margaret', 'Individual'),('Dumping Donuts', 'Business'), ('Ivy Cafe','Business'), ('1793678228', 'Business')")
 mycursor.execute("INSERT INTO Announcements(Location, Message, PAX) VALUES('Bukit Panjang', 'Extra rice left over at store, up to 5 people can  take', 5), ('King Albert Park', 'Extra prata remaining', 2), ('Choa Chu Kang', 'Extra chicken remaining', 3)")
-# mycursor.execute("INSERT INTO Users(User, Type) VALUES('Ben', 'Individual'),('Thomas', 'Individual'),('Margaret', 'Individual'),('Dumping Donuts', 'Business'), ('Ivy Cafe','Business')")
-# mycursor.execute("INSERT INTO Announcements(Location, Message, PAX) VALUES('Bukit Panjang', 'Extra rice left over at store, up to 5 people can  take', 5), ('King Albert Park', 'Extra prata remaining', 2), ('Choa Chu Kang', 'Extra chicken remaining', 3)")
-
-# execute your query 
 mycursor.execute("SELECT * FROM Users") 
   
 # fetch all the matching rows  
-result = mycursor.fetchall() 
+usr = mycursor.fetchall() 
   
 # loop through the rows 
 print("Users Table:")
-for row in result: 
+for row in usr: 
     print(row)
-    print("")
+
+mycursor.execute("SELECT * FROM Announcements") 
+  
+# fetch all the matching rows  
+ann = mycursor.fetchall() 
+  
+# loop through the rows 
+print("Announcements Table:")
+for row in ann: 
+    print(row)
    
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -48,30 +53,32 @@ TOKEN = os.getenv('TOKEN')
 
 ## define commands
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to Sustainibles! Run /join to get started!")
+    user_id = str(update.effective_user.id)
+    print("DEBUG UID: ", user_id)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to SustaiNibbles! Run /join to get started!")
 
-# Define states
+# /announce
 LOCATION, MESSAGE, PAX = range(3)
 
 async def announce(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Start the announcement conversation."""
+    """start announcement conversation, store location"""
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Please enter the location:")
     return LOCATION
 
 async def location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Store the location and ask for the message."""
+    """store the location, ask for the message"""
     context.user_data['location'] = update.message.text
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Please enter the description:")
     return MESSAGE
 
 async def message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Store the message and ask for the pax."""
+    """store the message, ask for the pax"""
     context.user_data['message'] = update.message.text
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Please enter the pax:")
     return PAX
 
 async def pax(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Store the pax and make the announcement."""
+    """store the pax, add announcement to database"""
     context.user_data['pax'] = update.message.text
     user_id = str(update.effective_user.id)
     print("DEBUG UID: ", user_id)
@@ -84,10 +91,19 @@ async def pax(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         location = context.user_data['location']
         message = context.user_data['message']
         pax = context.user_data['pax']
-        print("DEBUG: %s; %s; %s", location, message, pax)
-        mycursor.execute("INSERT INTO announcements (Location, Message, PAX) VALUES (%s, %s, %s)", (location, message, pax))
+        print(f"DEBUG: {location}, {message}, {pax}")
+        mycursor.execute("INSERT INTO Announcements (Location, Message, PAX) VALUES (%s, %s, %s)", (location, message, pax))
+        mycursor.execute("SELECT * FROM Announcements") 
+  
+        # fetch all the matching rows  
+        ann = mycursor.fetchall() 
         
+        # loop through the rows 
+        print("Announcements Table:")
+        for row in ann: 
+            print(row)
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Announcement has been made!")
+        
     else:
         await context.bot.send_message(chat_id=update.effective_chat.id, text="You do not have permission to use this command.")
     return ConversationHandler.END
@@ -98,11 +114,8 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
     return ConversationHandler.END
 
 def main() -> None:
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
     application = Application.builder().token(TOKEN).build()
 
-    # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
 
     conv_handler = ConversationHandler(
@@ -114,13 +127,8 @@ def main() -> None:
         },
         fallbacks=[CommandHandler('cancel', cancel)],
     )
-    
     application.add_handler(conv_handler)
     
-    # on non command i.e message - echo the message on Telegram
-    #application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == '__main__':
