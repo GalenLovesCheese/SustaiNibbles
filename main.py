7d3bc827f11d0e12c68f6d400581c760d1db1b29
FakeQwek <133119964+FakeQwek@users.noreply.github.com>
2024-12-21 20:32:40 +0800

    fix: Changed invalid parameter from User to Name

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ for database in databases:
 
 if databaseExists == False:
     mycursor.execute("CREATE DATABASE sustainibbles")
-    mycursor.execute("CREATE TABLE Users (User VARCHAR(255), Type VARCHAR(255) CHECK(Type = 'Individual' OR Type = 'Business'))")
+    mycursor.execute("CREATE TABLE Users (Name VARCHAR(255), Type VARCHAR(255) CHECK(Type = 'Individual' OR Type = 'Business'))")
     mycursor.execute("CREATE TABLE Announcements (Location VARCHAR(255), Message VARCHAR(255), PAX int)")
     mycursor.execute("INSERT INTO Users(User, Type) VALUES('Ben', 'Individual'),('Thomas', 'Individual'),('Margaret', 'Individual'),('Dumping Donuts', 'Business'), ('Ivy Cafe','Business')")
     mycursor.execute("INSERT INTO Announcements(Location, Message, PAX) VALUES('Bukit Panjang', 'Extra rice left over at store, up to 5 people can  take', 5), ('King Albert Park', 'Extra prata remaining', 2), ('Choa Chu Kang', 'Extra chicken remaining', 3)")
